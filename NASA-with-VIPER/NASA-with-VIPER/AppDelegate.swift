9914dc28bf2a74c9ceac97b1ce71a45ba99e5c3a
Arturo Carretero Calvo <10163049+ArtCC@users.noreply.github.com>
2018-09-20 18:57:14 +0200

    Config project

---
+++ NASA-with-VIPER/NASA-with-VIPER/AppDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.swift
+//  NASA-with-VIPER
+//
+//  Created by Arturo Carretero Calvo on 13/9/18.
+//  Copyright © 2018 Arturo Carretero Calvo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+}
---
+++ NASA-with-VIPER/NASA-with-VIPER/Scenes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NASA-with-VIPER
+//
+//  Created by Arturo Carretero Calvo on 13/9/18.
+//  Copyright © 2018 Arturo Carretero Calvo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
